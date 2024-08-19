8f9eb7bbca292fc121fd9171dfa34a9b7e3c4e70
moonhr <moonhr153@gmail.com>
2024-08-19 15:40:19 +0900

    Test | :)

---
+++ .next/server/server-reference-manifest.js
@@ -0,0 +1,1 @@
+self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"vbcxofN+6bjeGeVFjJRRWsottNwrkHM7tb7bv+TXdhM=\"}"
--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  experimental: {
+    appDir: true, // 앱 디렉토리 방식 활성화
+  },
+};
 
 export default nextConfig;
